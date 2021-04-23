af970cba0b867bcc5d69624f10f0d09bd3c976b7
Sunny  Prakash <sunny.pprakash@gmail.com>
2021-04-23 21:28:42 +0530

    added necessary tags

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 const App = () => {
-  return (
-    <div id="main"></div>
-  )
-}
-
+    return (
+        <div id="main">
+            <h1 data-ns-test="project-name">BookKeeper</h1>
+            <p data-ns-test="project-description">A website for book lovers.</p>
+        </div>
+    );
+};
 
 export default App;
